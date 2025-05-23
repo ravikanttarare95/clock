878e2ac034491f9d4f7c648ee5051573b70b7003
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-23 18:59:45 +0530

    Implimented Logic to 12/24hr buttom

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const datePara = document.getElementById("date-para");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
+let amPM = document.getElementById("am-pm");
 const days = [
   "Sunday",
   "Monday",
@@ -27,21 +28,37 @@ const months = [
   "November",
   "December",
 ];
+
+let is24HourFormat = true;
+
 setInterval(() => {
   let currentTime = new Date();
 
-  const h = String(currentTime.getHours()).padStart(2, "0");
+  let h = currentTime.getHours();
   const m = String(currentTime.getMinutes()).padStart(2, "0");
   const s = String(currentTime.getSeconds()).padStart(2, "0");
 
-  hours.innerHTML = `${h}`;
+  let period = "";
+
+  if (!is24HourFormat) {
+    period = h >= 12 ? "pm" : "am";
+    h = h % 12 || 12;
+    amPM.style.display = "inline";
+  } else {
+    amPM.style.display = "none";
+  }
+
+  const hours12 = String(h).padStart(2, "0");
+
+  hours.innerHTML = `${hours12}`;
   minutes.innerHTML = `${m}`;
   seconds.innerHTML = `${s}`;
+  amPM.innerText = is24HourFormat ? "" : `${period}`;
 
   datePara.innerHTML = `${days[currentTime.getDay()]}, ${
     months[currentTime.getMonth()]
   } ${currentTime.getDate()}, ${currentTime.getFullYear()}`;
-}, 1000);
+}, 500);
 
 function getTheme() {
   return localStorage.getItem("theme");
@@ -67,4 +84,8 @@ function toggleTheme() {
     document.body.classList.add("dark-mode");
     localStorage.setItem("theme", "dark");
   }
+}
+
+function toggleHour() {
+  is24HourFormat = !is24HourFormat;
 }
